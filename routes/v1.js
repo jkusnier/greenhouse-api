c7cf93062588ace7f559dd3255e355559452a410
Jason Kusnier <jkusnier@gmail.com>
2014-09-14 16:42:02 -0400

    stubbed route for webhook call

--- routes/v1.js
+++ routes/v1.js
@@ -10,3 +10,12 @@ router.get('/humidity', function(req, res) {
   var db = req.db;
 
 });
+
+router.post('/data', function(req, res) {
+//	console.log(req);
+//	console.log(res);
+
+	res.status(200).send("OK");
+});
+
+module.exports = router;
