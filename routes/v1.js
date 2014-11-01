edc1e45294129ebed69c1c813d0b8874740f7528
Jason Kusnier <jkusnier@gmail.com>
2014-11-01 19:16:59 -0400

    fixed to pull full collection

--- routes/v1.js
+++ routes/v1.js
@@ -22,8 +22,8 @@ router.get('/devices', function (req, res) {
 
 router.get('/devices/:device_id/last_day', function (req, res) {
     var db = req.db;
-    db.collection('last_day').findOne({coreid: req.params.device_id}, {sort: {byMinute: 1}}, function (err, result) {
-        res.json(result);
+    db.collection('last_day').find({coreid: req.params.device_id}, {sort: {created_on: 1}}).toArray(function (err, items) {
+        res.json(items);
     });
 });
 
