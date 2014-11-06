322f453746ba51fc281672c4445de84433258db1
Jason Kusnier <jkusnier@gmail.com>
2014-11-05 19:48:40 -0500

    only pulls last 2 weeks by default

--- routes/v1.js
+++ routes/v1.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 
 router.use(function(req,res,next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -28,6 +29,14 @@ router.get('/devices/:device_id/data/fahrenheit', function (req, res) {
 });
 
 router.get('/devices/:device_id/hist/fahrenheit', function (req, res) {
+    var db = req.db;
+    var startDate = moment().subtract(14, 'days').startOf('day');
+    db.collection('hist_fahrenheit').find({coreid: req.params.device_id, time: {$gte: new Date(startDate)}}, {sort: {time: 1}}).toArray(function (err, items) {
+        res.json(items);
+    });
+})
+
+router.get('/devices/:device_id/hist/fahrenheit/all', function (req, res) {
     var db = req.db;
     db.collection('hist_fahrenheit').find({coreid: req.params.device_id}, {sort: {time: 1}}).toArray(function (err, items) {
         res.json(items);
