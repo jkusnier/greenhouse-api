52c4b5505fe2239632927fc9436da08850fdea8b
Jason Kusnier <jkusnier@gmail.com>
2014-11-08 21:25:47 -0500

    cleaded formatting

--- routes/v1.js
+++ routes/v1.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var moment = require('moment');
 
-router.use(function(req,res,next) {
+router.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     next();
 });
@@ -31,7 +31,10 @@ router.get('/devices/:device_id/data/fahrenheit', function (req, res) {
 router.get('/devices/:device_id/hist/fahrenheit', function (req, res) {
     var db = req.db;
     var startDate = moment().subtract(14, 'days').startOf('day');
-    db.collection('hist_fahrenheit').find({coreid: req.params.device_id, time: {$gte: new Date(startDate)}}, {sort: {time: 1}}).toArray(function (err, items) {
+    db.collection('hist_fahrenheit').find({
+        coreid: req.params.device_id,
+        time: {$gte: new Date(startDate)}
+    }, {sort: {time: 1}}).toArray(function (err, items) {
         res.json(items);
     });
 })
